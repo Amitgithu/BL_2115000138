24fb7eb19f5cbd15e22f9aa8b351fb0b1c590f5c
Amit Kumar <amit909563@gmail.com>
2025-01-16 15:40:32 +0530

    Added: New Feature.js file

---
+++ feature.js
@@ -0,0 +1,12 @@
+console.log("Feature Branch file start");
+
+function sum(a, b){
+    console.log("Addition of two number ", a , " and ", b , " is : ", a + b);
+    
+    return a + b;
+}
+
+const sum = sum(10, 20);
+
+console.log("Feature Branch file ended");
+
